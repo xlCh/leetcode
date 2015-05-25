faf3ee531cc005787e33bca09e198a42cd81a656
xlCh <326591880@qq.com>
2015-05-25 19:27:20 +0800

    Create 066 plus one.cpp

---
+++ 066 plus one.cpp
@@ -0,0 +1,27 @@
+//+1操作
+vector<int> plusOne(vector<int>& digits) {
+        if (digits.size() == 0) {
+            return digits;
+        }
+        int i = digits.size() - 1;
+        if(digits[i] != 9) {
+            digits[i] += 1;
+            return digits;
+        }
+        while(digits[i] == 9 && i != 0) {
+            digits[i--] = 0;
+        }
+        if(i != 0) {
+            digits[i] += 1;
+            return digits;
+        }
+        else if(i == 0 && digits[i] == 9) {
+            digits[i] = 0;
+            digits.insert(digits.begin(), 1);
+            return digits;
+        }
+        else {
+            digits[i] += 1;
+            return digits;
+        }
+    }
