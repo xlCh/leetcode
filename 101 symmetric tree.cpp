319f3024d6d665a1a09ff41f2de01bad00156e61
xlCh <326591880@qq.com>
2015-05-26 09:56:40 +0800

    Create 101 symmetric tree.cpp

---
+++ 101 symmetric tree.cpp
@@ -0,0 +1,17 @@
+bool isSymmetric(TreeNode* root) {
+        if(root == NULL)
+            return true;
+        return areSymmetric(root->left, root->right);
+    }
+    
+    bool areSymmetric(TreeNode* t1, TreeNode* t2) {
+        if(t1 == NULL && t2 == NULL)
+            return true;
+        else if(t1 == NULL || t2 == NULL)
+            return false;
+        else if(t1->val != t2->val)
+            return false;
+        else {
+            return areSymmetric(t1->left, t2->right) && areSymmetric(t1->right, t2->left);
+        }
+    }
