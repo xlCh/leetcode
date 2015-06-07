0f68bcc38caaf7565c3a66a1b2aea782e5a7ccde
xlCh <326591880@qq.com>
2015-06-07 21:48:58 +0800

    Create 019 Remove Nth Node From End of List.cpp

---
+++ 019 Remove Nth Node From End of List.cpp
@@ -0,0 +1,29 @@
+//移除从末尾数第N个节点
+//方法有点冗余 待改进
+ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode * n1 = head;
+        ListNode * n2 = head;
+        bool isFirst = true;
+        for(int i=0; i<n; i++)
+            n1 = n1->next;
+        if(n1 == NULL) {
+            ListNode * p = head;
+            head = head->next;
+            free(p);
+            return head;
+        }
+        while(n1 != NULL) {
+            n1 = n1->next;
+            if(isFirst)
+                isFirst = false;
+            else
+                n2 = n2->next;
+        }
+        ListNode * temp = n2->next;
+        if(temp != NULL)
+            n2->next = temp->next;
+        else
+            n2->next = NULL;
+        free(temp);
+        return head;
+    }
