cfe3a649ac15aa5d816fa2f51a47e5fc139352b1
xlCh <326591880@qq.com>
2015-06-11 22:40:34 +0800

    Create 171 Excel Sheet Column Number.cpp

---
+++ 171 Excel Sheet Column Number.cpp
@@ -0,0 +1,8 @@
+//输出excel中列代表的数字（例如A=1,AA=27...）
+int titleToNumber(string s) {
+        int sum = 0;
+        for(int i=s.length()-1; i>=0; i--) {
+            sum = sum + ((s.at(i)-64) * pow(26,(s.length()-i-1)));
+        }
+        return sum;
+    }
