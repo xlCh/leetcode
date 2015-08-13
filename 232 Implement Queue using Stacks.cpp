9531d5ecf3d226f773ecfc0f5d2d74e80349c27c
xlCh <326591880@qq.com>
2015-08-13 23:01:57 +0800

    Create 232 Implement Queue using Stacks.cpp

---
+++ 232 Implement Queue using Stacks.cpp
@@ -0,0 +1,33 @@
+// 用两个栈实现一个队列
+stack<int> st1;
+    stack<int> st2;
+    // Push element x to the back of queue.
+    void push(int x) {
+        while(!st1.empty()) {
+            int temp = st1.top();
+            st1.pop();
+            st2.push(temp);
+        }
+        st2.push(x);
+        while(!st2.empty()) {
+            int temp = st2.top();
+            st2.pop();
+            st1.push(temp);
+        }
+        
+    }
+
+    // Removes the element from in front of queue.
+    void pop(void) {
+        st1.pop();
+    }
+
+    // Get the front element.
+    int peek(void) {
+        return st1.top();
+    }
+
+    // Return whether the queue is empty.
+    bool empty(void) {
+        return st1.empty();
+    }
