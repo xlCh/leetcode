67ed6917a5578965473a05d24372cb3ad7561a92
xlCh <326591880@qq.com>
2015-09-06 20:06:50 +0800

    Create 263 Ugly Number.cpp

---
+++ 263 Ugly Number.cpp
@@ -0,0 +1,24 @@
+//判断ugly number(只被2、3、5整除，1也算是ugly number)
+//有耗时更短方法 3个while  return num==1
+bool isUgly(int num) {
+        if(num == 0)
+            return false;
+        while(true) {
+            if(num == 1)
+                return true;
+            if(num % 2 == 0) {
+                num /= 2;
+                continue;
+            }
+            if(num % 3 == 0) {
+                num /= 3;
+                continue;
+            }
+            if(num % 5 == 0) {
+                num /= 5;
+                continue;
+            }
+            return false;
+        }
+        return true;
+    }
