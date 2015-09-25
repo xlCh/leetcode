30233e4c62ac0cdb73afec76ff3bb72b62bb1305
xlCh <326591880@qq.com>
2015-09-25 15:26:16 +0800

    Update 189 Rotate Array.java

--- 189 Rotate Array.java
+++ 189 Rotate Array.java
@@ -1,5 +1,5 @@
 //int数组整个右移k位
-//还有空间复杂度为O(1)的解法 待考虑
+//还有空间复杂度为O(1)的解法 一种是进行三次数组reverse（左、右、加起来），一种是逐步替换（需要考虑k和nums.length最大公约数不为1的情况）
 
 public void rotate(int[] nums, int k) {
         k = k % nums.length;
