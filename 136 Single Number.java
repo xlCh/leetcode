ffd051214bef84573278cf8c7e175d30a229991e
xlCh <326591880@qq.com>
2015-09-27 12:08:34 +0800

    Create 136 Single Number.java

---
+++ 136 Single Number.java
@@ -0,0 +1,10 @@
+//数组中只有一个数是单独出现的，其他都是成对出现，求这个数
+//利用0^N=N, N^N=0
+
+public int singleNumber(int[] nums) {
+        int single = 0;
+        for(int num : nums) {
+            single ^= num;
+        }
+        return single;
+    }
