8d76085f777014dbb97d94892d44fc8c077130f4
xlCh <326591880@qq.com>
2015-10-08 19:41:03 +0800

    Create 290 Word Pattern.java

---
+++ 290 Word Pattern.java
@@ -0,0 +1,22 @@
+//判断str是否符合pattern
+
+public boolean wordPattern(String pattern, String str) {
+        String[] words = str.split(" ");
+        Map<Character, String> mapping = new HashMap<Character, String>();
+        if(pattern.length() != words.length)
+            return false;
+        for(int i=0; i<pattern.length(); i++) {
+            Character ch = new Character(pattern.charAt(i));
+            if(mapping.containsKey(ch)) {
+                if(!words[i].equals(mapping.get(ch)))
+                    return false;
+            }
+            else if(mapping.containsValue(words[i])) {
+                return false;
+            }
+            else {
+                mapping.put(ch, words[i]);
+            }
+        }
+        return true;
+    }
