646c2869fe86e024d0d8ac8650d68589b325028a
xlCh <326591880@qq.com>
2015-10-10 14:45:09 +0800

    Create 108 Convert Sorted Array to Binary Search Tree.java

---
+++ 108 Convert Sorted Array to Binary Search Tree.java
@@ -0,0 +1,19 @@
+//将有序数组转换为二叉搜索树
+//递归解法
+
+public TreeNode sortedArrayToBST(int[] nums) {
+        if(nums.length == 0)
+            return null;
+        return newBST(nums, 0, nums.length-1);
+    }
+    
+    public TreeNode newBST(int[] nums, int low, int high) {
+        if(low > high) {
+            return null;
+        }
+        int middle = (low + high) / 2;
+        TreeNode root = new TreeNode(nums[middle]);
+        root.left = newBST(nums, low, middle-1);
+        root.right = newBST(nums, middle+1, high);
+        return root;
+    }
