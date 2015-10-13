6a5f13189575597f39b3a37533a62583564925b6
xlCh <326591880@qq.com>
2015-10-13 23:36:56 +0800

    Create 150 Evaluate Reverse Polish Notation.java

---
+++ 150 Evaluate Reverse Polish Notation.java
@@ -0,0 +1,31 @@
+//实现运算（运算符号在两个数字后面）
+
+public int evalRPN(String[] tokens) {
+        Stack<Integer> tokenStack = new Stack<Integer>();
+        int temp;
+        for(int i=0; i<tokens.length; i++) {
+            switch(tokens[i]) {
+                case "+":
+                    temp = tokenStack.pop() + tokenStack.pop();
+                    tokenStack.push(temp);
+                    break;
+                case "-":
+                    temp = -tokenStack.pop() + tokenStack.pop();
+                    tokenStack.push(temp);
+                    break;
+                case "*":
+                    temp = tokenStack.pop() * tokenStack.pop();
+                    tokenStack.push(temp);
+                    break;
+                case "/":
+                    int divide = tokenStack.pop();
+                    temp = tokenStack.pop() / divide;
+                    tokenStack.push(temp);
+                    break;
+                default:
+                    tokenStack.push(Integer.parseInt(tokens[i]));
+                    break;
+            }
+        }
+        return tokenStack.peek();
+    }
