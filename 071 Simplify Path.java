be456b0154bdd40f01dd8480f99985144d7b3984
xlCh <326591880@qq.com>
2015-10-18 16:35:04 +0800

    Create 071 Simplify Path.java

---
+++ 071 Simplify Path.java
@@ -0,0 +1,31 @@
+//路径的化简
+
+public String simplifyPath(String path) {
+        Stack<String> paths = new Stack<String>();
+        int i=0;
+        while(i < path.length()) {
+            if(path.charAt(i) == '/') {
+                String str = "";
+                while(i < path.length() && path.charAt(i) == '/') {
+                    i++;
+                }
+                while(i != path.length() && path.charAt(i) != '/') {
+                    str += path.charAt(i);
+                    i++;
+                }
+                if(str.equals("."))
+                    continue;
+                else if(str.equals("..")) {
+                    if(!paths.isEmpty())
+                        paths.pop();
+                }
+                else if(!str.equals(""))
+                    paths.push(str);
+            }
+        }
+        String result = "";
+        while(!paths.isEmpty()) {
+            result = '/' + paths.pop() + result;
+        }
+        return result.equals("")?"/":result;
+    }
