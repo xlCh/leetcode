4ae3d52d9ee83c0396a9cb3504851abbc4e300aa
xlCh <326591880@qq.com>
2015-10-18 18:07:51 +0800

    Create 121 Best Time to Buy and Sell Stock.java

---
+++ 121 Best Time to Buy and Sell Stock.java
@@ -0,0 +1,13 @@
+//股票最佳买入和卖出时间
+
+public int maxProfit(int[] prices) {
+        if(prices.length == 0)
+            return 0;
+        int min = prices[0];
+        int result = 0;
+        for(int i=1; i<prices.length; i++) {
+            min = min>prices[i]?prices[i]:min;
+            result = result>prices[i]-min?result:prices[i]-min;
+        }
+        return result;
+    }
