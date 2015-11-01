1e79da7b431a1e21acee020b0da5889f3de9e94b
xlCh <326591880@qq.com>
2015-11-01 21:01:45 +0800

    Update 213 House Robber II.java

--- 213 House Robber II.java
+++ 213 House Robber II.java
@@ -10,16 +10,14 @@ public int rob(int[] nums) {
         int[] value = new int[nums.length-1];
         value[0] = nums[0];
         value[1] = nums[0] > nums[1]?nums[0]:nums[1];
-        for(int i=2; i<nums.length-1; i++) {
-            value[i] = value[i-2]+nums[i] > value[i-1] ? value[i-2]+nums[i] : value[i-1];
-        }
-        int max1 = value[nums.length-2];
         int[] value2 = new int[nums.length-1];
         value2[0] = nums[1];
         value2[1] = nums[1] > nums[2]?nums[1]:nums[2];
         for(int i=2; i<nums.length-1; i++) {
+            value[i] = value[i-2]+nums[i] > value[i-1] ? value[i-2]+nums[i] : value[i-1];
             value2[i] = value2[i-2]+nums[i+1] > value2[i-1] ? value2[i-2]+nums[i+1] : value2[i-1];
         }
+        int max1 = value[nums.length-2];
         int max2 = value2[nums.length-2];
         return max1>max2?max1:max2;
     }
