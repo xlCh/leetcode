ada21bb53602143b642c2847ab7e6dccb0e816a5
xlCh <326591880@qq.com>
2015-11-05 19:52:07 +0800

    Create 120 Triangle.java

---
+++ 120 Triangle.java
@@ -0,0 +1,24 @@
+//三角从上到下和的最小值
+
+public int minimumTotal(List<List<Integer>> triangle) {
+        int rows = triangle.size();
+        int[] minSums = new int[rows];
+        minSums[0] = triangle.get(0).get(0);
+        for(int i=1; i<rows; i++) {
+            List<Integer> row = triangle.get(i);
+            for(int j=i; j>=0; j--) {
+                if(j == 0)
+                    minSums[j] += row.get(j);
+                else if(j == i)
+                    minSums[j] = minSums[j-1] + row.get(j);
+                else 
+                    minSums[j] = Math.min(minSums[j], minSums[j-1]) + row.get(j);
+            }
+            
+        }
+        int min = minSums[0];
+        for(int i=0; i<rows; i++) {
+            min = Math.min(minSums[i], min);
+        }
+        return min;
+    }
