b457e70300ab225685d1e7bb10b00b8e8fb77cfe
xlCh <326591880@qq.com>
2015-11-09 15:04:10 +0800

    Create 016 3Sum Closest.java

---
+++ 016 3Sum Closest.java
@@ -0,0 +1,29 @@
+//数组中三个数相加 返回最接近target的值
+
+ public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int result = nums[0] + nums[1] + nums[2];
+        for(int i=0; i<nums.length;) {
+            int start = i+1, end = nums.length-1, temp = nums[i];
+            while(start < end) {
+                int sum = nums[i] + nums[start] + nums[end];
+                int temp1 = nums[start], temp2 = nums[end];
+                if(sum < target) {
+                	if(target - sum < Math.abs(target - result)) {
+                		result = sum;
+                	}
+                	while(start < end && nums[start] == temp1) start++;
+                }
+                else if(sum == target)
+                	return sum;
+                else {
+                	if(sum - target < Math.abs(target - result)) {
+                		result = sum;
+                	}
+                	while(start < end && nums[end] == temp2) end--;
+                }
+            }
+            while(i<nums.length && nums[i] == temp) i++;
+        }
+        return result;
+    }
