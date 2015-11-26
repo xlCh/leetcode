46f88014f713eabe320e556681c18fc673e6a744
xlCh <326591880@qq.com>
2015-11-26 13:36:47 +0800

    Create 055 Jump Game.java

---
+++ 055 Jump Game.java
@@ -0,0 +1,12 @@
+//Jump game 是否能跳到最后一个元素
+//还有一种方法是倒着遍历
+
+public boolean canJump(int[] nums) {
+        int maxJump = nums[0];
+        for(int i=0; i<=maxJump; i++) {
+            maxJump = Math.max(maxJump, i+nums[i]);
+            if(maxJump >= nums.length-1)
+                return true;
+        }
+        return false;
+    }
