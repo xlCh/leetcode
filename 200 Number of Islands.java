1583abff1d09ad1afee34d1035f38c25b58f39e0
xlCh <326591880@qq.com>
2015-12-02 21:04:01 +0800

    Create 200 Number of Islands.java

---
+++ 200 Number of Islands.java
@@ -0,0 +1,29 @@
+//计算岛的个数
+//移除的方法 未用到并查集、BFS、DFS
+
+public int numIslands(char[][] grid) {
+        if(grid == null || grid.length == 0)
+            return 0;
+        int count = 0;
+        int m = grid.length, n = grid[0].length;
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(grid[i][j] != '0') {
+                    count++;
+                    removeIsland(grid, m, n, i, j);
+                }
+            }
+        }
+        return count;
+    }
+    
+    public void removeIsland(char[][] grid, int m, int n, int i, int j) {
+        if(i<0 || j<0 || i==m || j==n || grid[i][j]=='0')
+            return;
+        grid[i][j] = '0';
+        removeIsland(grid, m, n, i+1, j);
+        removeIsland(grid, m, n, i, j+1);
+        removeIsland(grid, m, n, i-1, j);
+        removeIsland(grid, m, n, i, j-1);
+        
+    }
