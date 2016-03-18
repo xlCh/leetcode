ed7779ffd4ca9b91de5c9a2f288d89748d5cb13a
xlCh <326591880@qq.com>
2016-03-18 20:32:26 +0800

    Update and rename 013 Roman to Integer.java to B 013 Roman to Integer.java

--- B 013 Roman to Integer.java
+++ B 013 Roman to Integer.java
@@ -1,5 +1,7 @@
+//https://leetcode.com/problems/roman-to-integer/
 //将罗马数字转换为阿拉伯数字
 
+//用map慢一些
 public int romanToInt(String s) {
         Map<Character, Integer> mapping = new HashMap<Character, Integer>();
         mapping.put('I', 1);
@@ -19,4 +21,24 @@ public int romanToInt(String s) {
             }
         }
         return count;
+    }
+    
+    //另一种解法，getNum函数略 思路不太一样
+     public int romanToInt(String s) {
+        int result = 0;
+        int i;
+        for(i=s.length()-1; i>0; i--) {
+            int ati = getNum(s.charAt(i));
+            int ati_1 = getNum(s.charAt(i-1));
+            if(ati > ati_1)  {
+                result += (ati - ati_1);
+                i--;
+            }
+            else
+                result += ati;
+        }
+        if(i == 0)
+            result += getNum(s.charAt(0));
+        return result;
+        
     }
