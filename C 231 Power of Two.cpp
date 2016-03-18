5b3f161eda0cd325ac6e8bcc8cc5f8c64e2ed6a4
xlCh <326591880@qq.com>
2016-03-19 01:03:57 +0800

    Update and rename 231 Power of Two.cpp to C 231 Power of Two.cpp

--- C 231 Power of Two.cpp
+++ C 231 Power of Two.cpp
@@ -1,3 +1,4 @@
+//https://leetcode.com/problems/power-of-two/
 //判断整数是否为2的n次幂
 bool isPowerOfTwo(int n) {
         if(n <= 0)
@@ -9,3 +10,7 @@ bool isPowerOfTwo(int n) {
         }
         return true;
     }
+
+//位操作
+//return (n > 0) && ((n&(n-1)) == 0);  利用其特性
+//return n>0&&1073741824%n==0; 
