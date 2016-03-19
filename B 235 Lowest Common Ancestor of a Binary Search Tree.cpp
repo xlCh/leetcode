feaf2374676888e229120b052ceb52fd5257b21b
xlCh <326591880@qq.com>
2016-03-19 11:34:44 +0800

    Update and rename 235 Lowest Common Ancestor of a Binary Search Tree.cpp to B 235 Lowest Common Ancestor of a Binary Search Tree.cpp

--- B 235 Lowest Common Ancestor of a Binary Search Tree.cpp
+++ B 235 Lowest Common Ancestor of a Binary Search Tree.cpp
@@ -1,3 +1,4 @@
+//https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
 //找出二叉搜索树中两个节点最近的共同祖先
 //递归解法
 TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
@@ -9,4 +10,10 @@ TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
             return lowestCommonAncestor(root->left, p, q);
         else if(p->val > root->val && q->val > root->val)
             return lowestCommonAncestor(root->right, p, q);
+    }
+
+//非递归解法 与递归有些相似
+while(true){
+      if((root.val-p.val)*(root.val-q.val) <= 0) return root;
+      root = p.val < root.val ? root.left : root.right;
     }
