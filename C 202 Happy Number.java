fb814db359f34ade2a36955acbefa512339c18de
xlCh <326591880@qq.com>
2016-03-21 00:55:18 +0800

    Update and rename 202 Happy Number.java to C 202 Happy Number.java

--- C 202 Happy Number.java
+++ C 202 Happy Number.java
@@ -1,6 +1,7 @@
+//https://leetcode.com/problems/happy-number
 //happy number
 //每位上的平方 最终和为1，即为happy number
-
+//注意，退出条件：n < 10 && n != 1，这样不用额外借助数据结构 https://leetcode.com/discuss/87470/3-ms-java-solution-in-o-1-space
 public boolean isHappy(int n) {
         List<Integer> already = new ArrayList<Integer>();
         while(!already.contains(n)) {
