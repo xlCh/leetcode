5a78cc71354142980a7543d8c845e084a8ccdb0e
xlCh <326591880@qq.com>
2016-03-21 22:02:37 +0800

    Update and rename 110 Balanced Binary Tree.cpp to C 110 Balanced Binary Tree.cpp

--- C 110 Balanced Binary Tree.cpp
+++ C 110 Balanced Binary Tree.cpp
@@ -1,5 +1,6 @@
+//https://leetcode.com/problems/balanced-binary-tree/
 //判断二叉树是否平衡
-//递归解法，有优化空间
+//递归解法，有优化空间 可以避免两个递归函数 https://leetcode.com/discuss/86239/java-1ms-solution
 bool isBalanced(TreeNode* root) {
         if(root == NULL)
             return true;
