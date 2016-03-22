478639b74ed51ab4ee1b6c4a12bbc4b340a1d544
xlCh <326591880@qq.com>
2016-03-22 08:53:38 +0800

    Update and rename 014 longest common prefix.cpp to B 014 longest common prefix.cpp

--- B 014 longest common prefix.cpp
+++ B 014 longest common prefix.cpp
@@ -1,4 +1,6 @@
+//https://leetcode.com/problems/longest-common-prefix/
 //返回最长的相同前缀
+//另一种思路，直接比较整个前缀 https://leetcode.com/discuss/92804/1ms-easy-to-understand-java-solution
 string longestCommonPrefix(vector<string>& strs) {
         if (strs.size() == 0)
             return "";
@@ -22,4 +24,22 @@ string longestCommonPrefix(vector<string>& strs) {
             reStr.append(1, character);
         }
         return reStr;
+    }
+
+//简化了一些 java
+public String longestCommonPrefix(String[] strs) {
+        if(strs.length == 0)
+            return "";
+        int length = Integer.MAX_VALUE;
+        for(int i=0; i<strs.length; i++) {
+            length = Math.min(length, strs[i].length());
+        }
+        for(int i=0; i<length; i++) {
+            int ch = strs[0].charAt(i);
+            for(int j=1; j<strs.length; j++) {
+                if(strs[j].charAt(i) != ch)
+                    return strs[0].substring(0, i);
+            }
+        }
+        return strs[0].substring(0, length);
     }
