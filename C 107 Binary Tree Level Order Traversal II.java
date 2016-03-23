e2adc90914ad18816558b36e53f2699c6a0719bc
xlCh <326591880@qq.com>
2016-03-23 22:45:21 +0800

    Update and rename 107 Binary Tree Level Order Traversal II.java to C 107 Binary Tree Level Order Traversal II.java

--- C 107 Binary Tree Level Order Traversal II.java
+++ C 107 Binary Tree Level Order Traversal II.java
@@ -1,9 +1,10 @@
+//https://leetcode.com/problems/binary-tree-level-order-traversal-ii/
 //层次遍历 倒序（只在正序上稍作改动）
-
+//可正序后再Collections.reverse（递归解法快一些） 更快的是BFS：https://leetcode.com/discuss/81189/java-1ms-beats-98%25-using-preorder
 public List<List<Integer>> levelOrderBottom(TreeNode root) {
-        Stack<List<Integer>> levelStack = new Stack<List<Integer>>();
+        List<List<Integer>> result = new LinkedList<List<Integer>>();
         if(root == null)
-            return levelStack;
+            return result;
         Queue<TreeNode> queue1 = new LinkedList<TreeNode>();
         Queue<TreeNode> queue2 = new LinkedList<TreeNode>();
         queue1.add(root);
@@ -18,7 +19,7 @@ public List<List<Integer>> levelOrderBottom(TreeNode root) {
                 layer1.add(temp.val);
             }
             if(layer1.size() > 0)
-                levelStack.push(layer1);
+                result.add(0, layer1);
             List<Integer> layer2 = new ArrayList<Integer>();
             while(!queue2.isEmpty()) {
                 TreeNode temp = queue2.remove();
@@ -29,11 +30,7 @@ public List<List<Integer>> levelOrderBottom(TreeNode root) {
                 layer2.add(temp.val);
             }
             if(layer2.size() > 0)
-                levelStack.push(layer2);
+                result.add(0, layer2);
         }
-        List<List<Integer>> levels = new ArrayList<List<Integer>>();
-        while(!levelStack.isEmpty()) {
-            levels.add(levelStack.pop());
-        }
-        return levels;
+        return result;
     }
