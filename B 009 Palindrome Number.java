8af9b8d08f0e2b4c174834eec78b5de5c0d25434
xlCh <326591880@qq.com>
2016-03-23 11:31:17 +0800

    Update and rename 009 Palindrome Number.java to B 009 Palindrome Number.java

--- B 009 Palindrome Number.java
+++ B 009 Palindrome Number.java
@@ -1,5 +1,6 @@
+//https://leetcode.com/problems/palindrome-number/
 //判断数字是否为回文
-
+//另一种方法 反向 https://leetcode.com/discuss/91814/simple-java-o-1-space-solution
 public boolean isPalindrome(int x) {
         if(x < 0)
             return false;
@@ -10,7 +11,7 @@ public boolean isPalindrome(int x) {
             temp /= 10;
         }
         for(int i=digitCount; i>=0; i=i-2) {
-            int divide = (int)Math.pow(10, i);
+            int divide = (int)Math.pow(10, i); //放在最外层 快一点
             if(x % 10 != x/divide)
                 return false;
             x %= divide;
