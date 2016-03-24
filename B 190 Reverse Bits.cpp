7075db061a1a9941f0410ea0d5e304b9e0ebf183
xlCh <326591880@qq.com>
2016-03-24 18:53:12 +0800

    Update and rename 190 Reverse Bits.cpp to B 190 Reverse Bits.cpp

--- B 190 Reverse Bits.cpp
+++ B 190 Reverse Bits.cpp
@@ -1,3 +1,4 @@
+//https://leetcode.com/problems/reverse-bits/
 //求整数按二进制翻转后的数
 //例如43261596(00000010100101000001111010011100) -> 964176192(00111001011110000010100101000000)
 //速度更快的方法：位操作
@@ -11,5 +12,14 @@ uint32_t reverseBits(uint32_t n) {
             n = n/2;
         }
         result += pow(2, 31-i);
+        return result;
+    }
+
+//位操作 java
+    public int reverseBits(int n) {
+        int result = 0;
+        for(int i=0; i<32; i++) {
+            result += (((n >> i) & 1) << (31-i));
+        }
         return result;
     }
