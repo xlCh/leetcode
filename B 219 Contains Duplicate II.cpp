db6af1ad07670d6679c9ff1fa346b3f7c7c3b12c
xlCh <326591880@qq.com>
2016-03-24 17:56:35 +0800

    Update and rename 219 Contains Duplicate II.cpp to B 219 Contains Duplicate II.cpp

--- B 219 Contains Duplicate II.cpp
+++ B 219 Contains Duplicate II.cpp
@@ -1,5 +1,7 @@
+//https://leetcode.com/problems/contains-duplicate-ii/
 //判断距离k以内是否存在两个一样的元素
-//使用unordered_map，一定还有其他的巧妙方法
+//更快的方法：双数组一同参与排序https://leetcode.com/discuss/89129/5ms-98-8%25-java-solution
+//或者自造数据结构并实现Comparator接口，https://leetcode.com/discuss/88763/my-java-use-sort-beats-98-69%25-of-java-submissions
 bool containsNearbyDuplicate(vector<int>& nums, int k) {
         unordered_map<int, int> numMap;
         for(int i=0; i<nums.size(); i++) {
