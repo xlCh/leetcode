0fd99dccf78d8adc7ac694c2631b26ab36428fff
xlCh <326591880@qq.com>
2016-03-25 22:33:32 +0800

    Update and rename 035 Search Insert Position.java to B 035 Search Insert Position.java

--- B 035 Search Insert Position.java
+++ B 035 Search Insert Position.java
@@ -1,5 +1,6 @@
+//https://leetcode.com/problems/search-insert-position
 //寻找一个数在数组中的插入位置，如果存在则直接返回下标
-
+//更精简的代码 把start初始值设置为-1 https://leetcode.com/discuss/88085/standard-binary-search-template-maybe-better-than-other-posts
 public int searchInsert(int[] nums, int target) {
         int low = 0, high = nums.length-1;
         if(target < nums[low])
