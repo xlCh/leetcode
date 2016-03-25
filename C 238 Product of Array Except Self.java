8d64ff5e58d0c83c3229e37be13d6665d89a117a
xlCh <326591880@qq.com>
2016-03-25 21:32:10 +0800

    Update and rename 238 Product of Array Except Self.java to C 238 Product of Array Except Self.java

--- C 238 Product of Array Except Self.java
+++ C 238 Product of Array Except Self.java
@@ -1,7 +1,7 @@
+//https://leetcode.com/problems/product-of-array-except-self/
 //数组中除自己之外其他元素的乘积 不能用除法
-
 //分别从左边和右边开始乘
-
+//可以在一个循环中搞定 https://leetcode.com/discuss/87865/how-from-o-n-to-o-1
 public int[] productExceptSelf(int[] nums) {
         int[] result = new int[nums.length];
         result[0] = 1;
