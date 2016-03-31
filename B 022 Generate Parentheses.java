7a3ce152a5375cda215470dc5fafd1f79e082407
xlCh <326591880@qq.com>
2016-03-31 22:52:49 +0800

    Update and rename 022 Generate Parentheses.java to B 022 Generate Parentheses.java

--- B 022 Generate Parentheses.java
+++ B 022 Generate Parentheses.java
@@ -1,5 +1,7 @@
+//https://leetcode.com/problems/generate-parentheses/
 //给定n组左右括号 求组合
-
+//generate可以只有三个参数：str, left, right
+//StringBuilder反而不如直接用String
 public class Solution {
     List<String> result = new ArrayList<String>();
     public List<String> generateParenthesis(int n) {
