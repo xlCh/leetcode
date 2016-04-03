a4f9ed16324528fdd6cee5099d5e95e8162cd08a
xlCh <326591880@qq.com>
2016-04-03 15:35:38 +0800

    Update and rename 075 Sort Colors.java to C 075 Sort Colors.java

--- C 075 Sort Colors.java
+++ C 075 Sort Colors.java
@@ -1,5 +1,6 @@
+//https://leetcode.com/problems/sort-colors/
 //三种颜色排序 不占用额外空间的方法
-
+//思考k种颜色排序：https://leetcode.com/discuss/93302/sort-colors-and-sort-k-colors-c-solution
 public void sortColors(int[] nums) {
         int zero = 0, temp = 0, two = nums.length-1;
         while(temp <= two) {
@@ -8,7 +9,7 @@ public void sortColors(int[] nums) {
                 nums[zero++] = 0;
             }
             else if(nums[temp] == 2) {
-                nums[temp] = nums[two];
+                nums[temp] = nums[two];//注意差别
                 nums[two--] = 2;
             }
             else
