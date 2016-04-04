d8652dab8dda50bc005c1a875bc0297be72eeefe
xlCh <326591880@qq.com>
2016-04-04 21:46:55 +0800

    Update and rename 114 Flatten Binary Tree to Linked List.java to C 114 Flatten Binary Tree to Linked List.java

--- C 114 Flatten Binary Tree to Linked List.java
+++ C 114 Flatten Binary Tree to Linked List.java
@@ -1,5 +1,6 @@
+//https://leetcode.com/problems/flatten-binary-tree-to-linked-list
 //将二叉树转换为只含有向右的指针（相当于前序遍历的顺序）
-
+//多种实现形式
 public void flatten(TreeNode root) {
         if(root == null)
             return;
