d1c7a4d77c8acf53e48622005f95e523bd338213
xlCh <326591880@qq.com>
2016-04-04 21:35:13 +0800

    Update and rename 274 H-Index.java to D 274 H-Index.java

--- D 274 H-Index.java
+++ D 274 H-Index.java
@@ -1,6 +1,7 @@
-//计算h-index https://en.wikipedia.org/wiki/H-index、
-//另一种方法不排序 占用额外空间
-
+//https://leetcode.com/problems/h-index/
+//计算h-index https://en.wikipedia.org/wiki/H-index
+//另一种方法不排序 占用额外空间，转化思想https://leetcode.com/discuss/93819/java-bucket-sort-o-n-solution-with-detail-explanation
+//还有一种方法 更简单https://leetcode.com/discuss/79740/according-to-the-definition-of-h-index-in-wiki-it-is-simple
 public int hIndex(int[] citations) {
         Arrays.sort(citations);
         int length = citations.length;
