1034a7b9cfd761c1ec555b0e8d41fbed6b17865f
xlCh <326591880@qq.com>
2016-04-06 22:28:00 +0800

    Update and rename 053 Maximum Subarray.java to C 053 Maximum Subarray.java

--- C 053 Maximum Subarray.java
+++ C 053 Maximum Subarray.java
@@ -1,5 +1,8 @@
+//https://leetcode.com/problems/maximum-subarray/
 //寻找最大子串
-
+//另一种类似buy and sell stock的解法：https://leetcode.com/discuss/83182/another-o-n-solution
+//分治解法：https://leetcode.com/discuss/13569/share-my-solutions-both-greedy-and-divide-and-conquer
+//https://leetcode.com/discuss/694/how-solve-maximum-subarray-using-divide-and-conquer-approach
 public int maxSubArray(int[] nums) {
         int sum = nums[0];
         int maxSum = sum;
