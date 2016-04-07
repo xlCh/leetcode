eb2345be4be413465268a64979817ebe2f55186d
xlCh <326591880@qq.com>
2016-04-07 22:31:17 +0800

    Update and rename 077 Combinations.java to B 077 Combinations.java

--- B 077 Combinations.java
+++ B 077 Combinations.java
@@ -1,3 +1,4 @@
+//https://leetcode.com/problems/combinations/
 //从n个中选取k个的组合
 
 public class Solution {
@@ -18,6 +19,29 @@ public class Solution {
             List<Integer> newResult = new ArrayList<Integer>(result);
             newResult.add(i);
             combineNums(newResult, i+1, n, k-1);
+        }
+    }
+}
+
+//更快的方法
+public class Solution {
+    
+    List<List<Integer>> result = new ArrayList<List<Integer>>();
+    
+    public List<List<Integer>> combine(int n, int k) {
+        combine(new ArrayList<Integer>(), n, k, 1);
+        return result;
+    }
+    
+    public void combine(List<Integer> already, int n, int k, int now) {
+        if(already.size() == k) {
+            result.add(new ArrayList(already));
+            return;
+        }
+        for(int i=now; i<=n; i++) {
+                already.add(i);
+                combine(already, n, k, i+1);
+                already.remove(already.size()-1);
         }
     }
 }
