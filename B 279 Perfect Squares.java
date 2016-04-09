304870dc05cabc053668bdbcd15bbde34fe93eed
xlCh <326591880@qq.com>
2016-04-09 15:49:34 +0800

    Update and rename 279 Perfect Squares.java to B 279 Perfect Squares.java

--- B 279 Perfect Squares.java
+++ B 279 Perfect Squares.java
@@ -1,5 +1,8 @@
+//https://leetcode.com/problems/perfect-squares/
 //平方和 最小数字
-
+//dp解法
+//four-square理论：https://leetcode.com/discuss/87524/java-use-lagranges-four-square-theorem
+//https://leetcode.com/discuss/87524/java-use-lagranges-four-square-theorem
 public int numSquares(int n) {
         int[] minNums = new int[n+1];
         for(int i=1; i<=n; i++) {
