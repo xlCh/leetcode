5b8bc143330434d1d309c9cc5e03a14f02a4baef
xlCh <326591880@qq.com>
2016-04-11 21:10:24 +0800

    Update and rename 147 Insertion Sort List.java to B 147 Insertion Sort List.java

--- B 147 Insertion Sort List.java
+++ B 147 Insertion Sort List.java
@@ -1,5 +1,6 @@
-//链表的插入排序
-
+//https://leetcode.com/problems/insertion-sort-list/
+//链表的插入排序，但效率不够高
+//一种高效的方法，利用链表的特性，免除过多的比较：https://leetcode.com/discuss/92229/7ms-java-solution-with-explanation
 public ListNode insertionSortList(ListNode head) {
         ListNode newHead = new ListNode(Integer.MIN_VALUE);
         ListNode current = head;
