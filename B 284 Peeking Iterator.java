e3a40f34649a832c33582b7ac322aa04aeb723a4
xlCh <326591880@qq.com>
2016-04-12 10:52:14 +0800

    Update and rename 284 Peeking Iterator.java to B 284 Peeking Iterator.java

--- B 284 Peeking Iterator.java
+++ B 284 Peeking Iterator.java
@@ -1,6 +1,7 @@
+//https://leetcode.com/problems/peeking-iterator/
 //设计可以预读取一个元素的类
 //预读取时首先调用next 真正调用next的时候判断是否已调用peek
-
+//另一种方法，行数少一些：https://docs.oracle.com/javase/8/docs/api/java/util/Iterator.html
 class PeekingIterator implements Iterator<Integer> {
 
     private boolean isPeeked;
