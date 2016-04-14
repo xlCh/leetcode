583ae7d709960056b54951dcb2664b28235b8d55
xlCh <326591880@qq.com>
2016-04-14 20:07:13 +0800

    Update and rename 133 Clone Graph.java to C 133 Clone Graph.java

--- C 133 Clone Graph.java
+++ C 133 Clone Graph.java
@@ -1,6 +1,7 @@
+//https://leetcode.com/problems/clone-graph/
 //复制图
 //深度优先遍历 递归解法
-
+//非递归解法 BFS：https://leetcode.com/discuss/80503/java-bfs-with-comments
 public class Solution {
     
     private Map<UndirectedGraphNode, UndirectedGraphNode> mapping = new HashMap<UndirectedGraphNode, UndirectedGraphNode>();
