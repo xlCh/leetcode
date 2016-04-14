327a96f7080921de76a3b81ff82ad3648bbddf4a
xlCh <326591880@qq.com>
2016-04-14 14:23:20 +0800

    Update and rename 092 Reverse Linked List II.java to A 092 Reverse Linked List II.java

---
+++ A 092 Reverse Linked List II.java
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/reverse-linked-list-ii/
+//倒转链表中m~n位置的元素
+
+ public ListNode reverseBetween(ListNode head, int m, int n) {
+        ListNode newHead = new ListNode(0);
+        newHead.next = head;
+        ListNode rHead = newHead, rTail = newHead;
+        for(int i=1; i<m; i++) {
+        	rHead = rHead.next;
+        	rTail = rTail.next;
+        }
+        for(int i=m; i<=n+1; i++) {
+        	rTail = rTail.next;
+        }
+        ListNode reverse = rHead.next;
+        rHead.next = rTail;
+        while(reverse != rTail) {
+        	ListNode temp = reverse.next;
+        	reverse.next = rHead.next;
+        	rHead.next = reverse;
+        	reverse = temp;
+        }
+        //reverse = rTail;
+        return newHead.next;
+    }
