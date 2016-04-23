2b01a17716df1a111b29e5b0d55e19e8d2ea8ae2
xlCh <326591880@qq.com>
2016-04-23 16:07:35 +0800

    Update and rename 047 Permutations II.java to C 047 Permutations II.java

--- C 047 Permutations II.java
+++ C 047 Permutations II.java
@@ -1,3 +1,4 @@
+//https://leetcode.com/problems/permutations-ii/
 //数组的所有排列次序（有重复元素 不允许重复）
 //仅对Permutation做简单修改 时间慢
 //https://leetcode.com/discuss/58270/short-fast-recursive-solution-understand-with-explaination
@@ -27,3 +28,32 @@ public class Solution {
         }
     }
 }
+//https://leetcode.com/discuss/25279/a-simple-c-solution-in-only-20-lines 以及回复
+//较快 注意copyOf那一段，不使用会出错
+List<List<Integer>> result = new ArrayList<List<Integer>>();
+    
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        Arrays.sort(nums);
+        permute(nums, 0);
+        return result;
+    }
+    
+    public void permute(int[] nums, int start) {
+        if(start == nums.length-1) {
+            List<Integer> temp = new ArrayList<Integer>();
+            for(int num : nums)
+                temp.add(num);
+            result.add(temp);
+            return;
+        }
+        for(int i=start; i<nums.length; i++) {
+            if(i != start && nums[start] == nums[i])
+                continue;
+            int temp = nums[start];
+            nums[start] = nums[i];
+            nums[i] = temp;
+            permute(Arrays.copyOf(nums, nums.length), start+1);
+            //nums[i] = nums[start];
+           // nums[start] = temp;
+        }
+    }
