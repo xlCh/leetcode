8bec8724002f91dc822e5826a088a455b1559c48
xlCh <326591880@qq.com>
2016-05-04 15:16:40 +0800

    Update and rename 050 Pow(x, n).java to C 050 Pow(x, n).java

--- C 050 Pow(x, n).java
+++ C 050 Pow(x, n).java
@@ -1,6 +1,8 @@
 //计算x的n次幂
 //注意从n开始除 如果从1开始乘 数大会溢出
 //位操作解法：https://leetcode.com/discuss/12004/my-answer-using-bit-operation-c-implementation
+//递归解法：https://leetcode.com/discuss/73885/exponentiation-by-squaring-java-1ms-ac
+//不需要转换long，直接考虑负数（下面的评论） https://leetcode.com/discuss/85636/similar-idea-with-binary
 
 public double myPow(double x, int n) {
         if(x == 0)
@@ -19,4 +21,23 @@ public double myPow(double x, int n) {
             n /= 2;
         }
         return result;
+    }
+
+//注意MIN_VALUE
+public double myPow(double x, int n) {
+        if(n == 0 || x == 1)
+			return 1;
+		double result = 1;
+		long m = n;
+		if(m < 0) {
+		    x = 1 / x;
+		    m = -m;
+		}
+		while(m >= 1) {
+		    if(m % 2 == 1)
+		        result *= x;
+		    m >>= 1;
+		    x *= x;
+		}
+		return result;
     }
