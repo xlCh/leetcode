1ec6719b82af45ac98ca9522affdda028e502796
xlCh <326591880@qq.com>
2016-05-06 22:45:31 +0800

    Update and rename 007 Reverse Integer.java to B 007 Reverse Integer.java

--- B 007 Reverse Integer.java
+++ B 007 Reverse Integer.java
@@ -1,3 +1,4 @@
+//https://leetcode.com/problems/reverse-integer/
 //将int翻转，溢出返回0
 //注意加法和乘法都可能溢出
 
@@ -15,6 +16,19 @@ public int reverse(int x) {
         		return 0;
         	result += times * everyBit.get(i);
         	times *= 10;
+        }
+        return result;
+    }
+
+//更简短一些
+public int reverse(int x) {
+        int result = 0;
+        while(x != 0) {
+            //应该还可以简化
+            if((x > 0 && (Integer.MAX_VALUE - x % 10) / 10 < result) || (x < 0 && (Integer.MIN_VALUE - x % 10) / 10 > result))
+                return 0;
+            result = 10 * result + x % 10;
+            x /= 10;
         }
         return result;
     }
