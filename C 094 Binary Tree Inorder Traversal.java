8dc9f7b0200d8d21fcf786c75ec541ab6b5919ff
xlCh <326591880@qq.com>
2016-05-06 16:00:27 +0800

    Update and rename 094 Binary Tree Inorder Traversal.java to C 094 Binary Tree Inorder Traversal.java

---
+++ C 094 Binary Tree Inorder Traversal.java
@@ -0,0 +1,36 @@
+//https://leetcode.com/problems/binary-tree-inorder-traversal/
+//二叉树的中序遍历 递归解法
+
+public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> traverse = new ArrayList<Integer>();
+        inorder(root, traverse);
+        return traverse;
+    }
+    
+    public void inorder(TreeNode root, List<Integer> traverse) {
+        if(root == null)
+            return;
+        inorder(root.left, traverse);
+        traverse.add(root.val);
+        inorder(root.right, traverse);
+    }
+
+//非递归写法
+public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+    	TreeNode cur = root, pre = null;
+    	while(!stack.isEmpty() || cur != null) {
+    		if(cur != null) {
+    			stack.push(cur);
+    			cur = cur.left;
+    		}
+    		else {
+    			TreeNode temp = stack.pop();
+    			result.add(temp.val);
+    			pre = temp;
+    			cur = temp.right;
+    		}
+    	}
+    	return result;
+    }
