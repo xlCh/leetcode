8a5bca8c53c425ce7a46be661243310ac74594f6
xlCh <326591880@qq.com>
2016-05-08 01:40:25 +0800

    Update and rename 089 Gray Code.java to D 089 Gray Code.java

--- D 089 Gray Code.java
+++ D 089 Gray Code.java
@@ -1,6 +1,9 @@
+//https://leetcode.com/problems/gray-code/
 //Gray Code
 //https://en.wikipedia.org/wiki/Gray_code
-
+//回溯解法：https://leetcode.com/discuss/74543/backtracking-c-solution
+//Gi = Bi+1 xor Bi 这个公式干嘛的 https://leetcode.com/discuss/13058/share-my-simple-way-of-this-problem-_
+//一般的按照对称性的解法如下：
 public List<Integer> grayCode(int n) {
         List<Integer> result = new ArrayList<Integer>();
         result.add(0);
