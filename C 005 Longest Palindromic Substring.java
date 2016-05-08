03deb86b99675f33b3670a1aae03efbe775a0de3
xlCh <326591880@qq.com>
2016-05-09 01:26:05 +0800

    Update and rename 005-Longest-Palindromic-Substring.java to C 005 Longest Palindromic Substring.java

--- C 005 Longest Palindromic Substring.java
+++ C 005 Longest Palindromic Substring.java
@@ -1,5 +1,6 @@
+//https://leetcode.com/problems/longest-palindromic-substring/
 //寻找字符串中最长回文 挨个找
-
+//看似是另一种方法：https://leetcode.com/discuss/9948/a-very-brief-o-n-time-o-1-space-solution-ac
 public class Solution {
     int paStart = 0, longest = 0;
     public String longestPalindrome(String s) {
