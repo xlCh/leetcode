2ddd5ba35737e342793c9d54c2b30d23c0989cfd
xlCh <326591880@qq.com>
2016-05-09 01:37:42 +0800

    Update and rename 222 Count Complete Tree Nodes.java to D 222 Count Complete Tree Nodes.java

--- D 222 Count Complete Tree Nodes.java
+++ D 222 Count Complete Tree Nodes.java
@@ -1,4 +1,9 @@
+//https://leetcode.com/problems/count-complete-tree-nodes/
 //计算完全二叉树中结点的个数
+//另一种递归解法 利用完全二叉树左右是否为满二叉树：https://leetcode.com/discuss/73892/a-very-clear-recursive-solution-isnt-it
+//更简洁的版本：https://leetcode.com/discuss/38930/concise-java-solutions-o-log-n-2
+//三种不同解法：https://leetcode.com/discuss/38930/concise-java-solutions-o-log-n-2
+//最快的解法：https://leetcode.com/discuss/73735/my-java-solution-with-explanation-which-beats-99%25
 //discuss中的递归解法
 //但将1 << leftHeight改成Math.pow会超时
 
