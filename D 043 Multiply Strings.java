4061cbb345f905ffaa3ce5636a8ebebf801d5a61
xlCh <326591880@qq.com>
2016-05-13 11:32:31 +0800

    Update and rename 043 Multiply Strings.java to D 043 Multiply Strings.java

--- D 043 Multiply Strings.java
+++ D 043 Multiply Strings.java
@@ -1,5 +1,7 @@
+//https://leetcode.com/problems/multiply-strings/
 //大数乘法  找规律
-
+//虽然快但看不太懂。。https://leetcode.com/discuss/87274/3ms-java-solution-beats-100%25
+//包含图示：https://leetcode.com/discuss/71593/easiest-java-solution-with-graph-explanation
 public String multiply(String num1, String num2) {
         int length1 = num1.length(), length2 = num2.length();
         int[] result = new int[length1 + length2];
