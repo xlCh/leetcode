c5e6d588a21cb651b180df578c854327d11a4d96
xlCh <326591880@qq.com>
2016-05-13 10:20:01 +0800

    Update and rename 152 Maximum Product Subarray.java to C 152 Maximum Product Subarray.java

--- C 152 Maximum Product Subarray.java
+++ C 152 Maximum Product Subarray.java
@@ -1,5 +1,6 @@
+//https://leetcode.com/problems/maximum-product-subarray/
 //数组中元素的最大乘积
-
+//大同小异的方法 https://leetcode.com/discuss/14235/possibly-simplest-solution-with-o-n-time-complexity
 public int maxProduct(int[] nums) {
         int max = nums[0], min = nums[0], result = nums[0];
         for(int i=1; i<nums.length; i++) {
@@ -15,4 +16,17 @@ public int maxProduct(int[] nums) {
             result = Math.max(result, max);
         }
         return result;
+    }
+
+//更直观一些
+public int maxProduct(int[] nums) {
+        int max = nums[0], min = nums[0], result = nums[0];
+		for(int i=1; i<nums.length; i++) {
+			int tempMax = max * nums[i], tempMin = min * nums[i];
+			max = Math.max(Math.max(tempMax, tempMin), nums[i]);
+			min = Math.min(Math.min(tempMax, tempMin), nums[i]);
+			if(result < max)
+				result = max;
+		}
+		return result;
     }
